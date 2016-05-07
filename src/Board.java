de66e6643b89768582f14bd97c64f167bbdcbf24
David Allen <dma@gnu.org>
2016-05-07 12:23:27 -0500

    Added Square as basic unit

--- src/Board.java
+++ src/Board.java
@@ -1,27 +1,40 @@
 public class Board {
-	private Piece[][] board = new Piece[9][9];
+	private Square[][] board = new Square[9][9];
 	
 	public Board() {
-		//Add Pawns
+		//Add Squares
+		for(int r=0;r<board.length;r++) {
+			for(int c=0;c<board[r].length;c++) {
+				board[r][c] = new Square(r,c);
+			}
+		}
+		//Add Pawns to initial setup
 		for(int i = 0; i < 9;i++) {
-			board[2][i] = new Pawn();
-			board[6][i] = new Pawn();
+			board[2][i].setPiece(new Pawn());
+			board[6][i].setPiece(new Pawn());
 		}
 	}
 	
-	public void movePiece(Piece p) {
-		
+	public void movePiece(Square from, Square to) {
+		Piece fromPiece = from.getPiece();
+		// if(fromPiece.canMove())
+		from.setPiece(null);
+		to.setPiece(fromPiece);
+	}
+	
+	public Square getSquare(int r, int c) {
+		return board[r][c];
 	}
 	
 	public String toString() {
 		String ret = "";
 		for(int r=0;r<board.length;r++) {
 			for(int c=0;c<board[r].length;c++) {
-				if(board[r][c] == null) {
+				if(board[r][c].getPiece() == null) {
 					ret += " + ";
 				} 
 				else {
-					ret += " " + board[r][c].getSymbol() + " ";
+					ret += " " + board[r][c].getPiece().getSymbol() + " ";
 				}
 			}
 			ret += "\n";
