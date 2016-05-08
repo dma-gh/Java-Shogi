f090165213cb974b1794d062c282efb414f67ce5
David Allen <dma@gnu.org>
2016-05-07 23:59:32 -0500

    Added King and move functionality

---
+++ src/King.java
@@ -0,0 +1,34 @@
+
+public class King implements Piece {
+	private String type = "King";
+	private String symbol = "K";
+	private int owner;
+	
+	public boolean canMove(Square from, Square to, Board b) {
+		if((Math.abs(from.getC() - to.getC()) == 1) ||
+				Math.abs(from.getR() - to.getR()) == 1) {	
+		return true;
+		}
+		return false;
+	}
+	
+	public King(int owner) {
+		this.owner = owner;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public String getSymbol() {
+		return symbol;
+	}
+	
+	public void setOwner(int owner) {
+		this.owner = owner;
+	}
+	
+	public int getOwner() {
+		return owner;
+	}
+}
