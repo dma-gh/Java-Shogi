86da579360f593e2d3300a42b10dfe24c174b8e4
David Allen <dma@gnu.org>
2016-05-08 03:21:59 -0500

    Added click movement functionality

--- src/Shogi.java
+++ src/Shogi.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,6 +16,7 @@ public class Shogi extends JFrame {
 	static JFrame frame = new JFrame();
 	static JButton[][] squares = new JButton[9][9];
 	static Board b = new Board();
+	static Square lastClicked = null;
 
 	public static void main(String[] args){
 
@@ -30,6 +33,25 @@ public class Shogi extends JFrame {
 				squares[r][c].setOpaque(true);
 				squares[r][c].setSize(54, 54);
 				squares[r][c].setBorder(new LineBorder(Color.BLACK));
+				squares[r][c].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						for(int r=0;r<9;r++) {
+							for(int c=0;c<9;c++) {
+								if(e.getSource() == squares[r][c]) {
+									if(lastClicked == null) {
+										if(b.getSquare(r, c).getPiece() != null) {
+											lastClicked = b.getSquare(r, c);
+										}
+									} else {
+										b.movePiece(lastClicked, b.getSquare(r, c));
+										lastClicked = null;
+										updateBoard();
+									}
+								}
+							}
+						}
+					}
+				});
 				boardGUI.add(squares[r][c]);
 			}
 		}
@@ -50,8 +72,13 @@ public class Shogi extends JFrame {
 						squares[r][c].setText(b.getSquare(r, c).getPiece().getSymbol());
 						squares[r][c].setForeground(Color.BLUE);
 					}
+				} else {
+					squares[r][c].setText("");
+					squares[r][c].setForeground(Color.BLACK);
 				}
 			}
 		}
+		frame.revalidate();
+		frame.repaint();
 	}
 }
