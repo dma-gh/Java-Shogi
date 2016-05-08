2b9e6ec21a5270534a82158e7924e25b9d7c2ad5
David Allen <dma@gnu.org>
2016-05-08 16:51:51 -0500

    Updated game mechanics

--- src/Shogi.java
+++ src/Shogi.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
