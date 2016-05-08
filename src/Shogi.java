e23bcd24356ba30b7897b45b75ae242605b081a8
David Allen <dma@gnu.org>
2016-05-08 00:18:29 -0500

    Basic king checking

--- src/Shogi.java
+++ src/Shogi.java
@@ -3,7 +3,7 @@ public class Shogi {
 	public static void main(String[] args) {
 		Board b = new Board();
 		System.out.println(b.toString());
-		b.movePiece(b.getSquare(2, 0), b.getSquare(3, 0));
+		b.movePiece(b.getSquare(8, 4), b.getSquare(7, 6));
 		System.out.println(b.toString());
 		b.movePiece(b.getSquare(6, 0), b.getSquare(5, 0));
 		System.out.println(b.toString());
