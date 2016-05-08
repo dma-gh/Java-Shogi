e2854262e7f585ff8af84bb71cf9bd0e7dcf46b8
David Allen <dma@gnu.org>
2016-05-08 00:23:41 -0500

    Finalized King piece

--- src/Shogi.java
+++ src/Shogi.java
@@ -3,7 +3,7 @@ public class Shogi {
 	public static void main(String[] args) {
 		Board b = new Board();
 		System.out.println(b.toString());
-		b.movePiece(b.getSquare(8, 4), b.getSquare(7, 6));
+		b.movePiece(b.getSquare(8, 4), b.getSquare(7, 5));
 		System.out.println(b.toString());
 		b.movePiece(b.getSquare(6, 0), b.getSquare(5, 0));
 		System.out.println(b.toString());
