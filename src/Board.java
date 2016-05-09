18c2235e1b8a8982c8ca9389e71645dc1f997fe2
David Allen <dma@gnu.org>
2016-05-08 20:39:49 -0500

    Allowed for drops in move method

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,6 @@
 public class Board {
 	private Square[][] board = new Square[9][9];
-	
+
 	public Board() {
 		//Add Squares
 		for(int r=0;r<board.length;r++) {
@@ -14,51 +14,57 @@ public class Board {
 			board[6][i].setPiece(new Pawn(2));
 		}
 		//Add Lances to initial setup
-			board[0][0].setPiece(new Lance(1));
-			board[0][8].setPiece(new Lance(1));
-			board[8][0].setPiece(new Lance(2));
-			board[8][8].setPiece(new Lance(2));
+		board[0][0].setPiece(new Lance(1));
+		board[0][8].setPiece(new Lance(1));
+		board[8][0].setPiece(new Lance(2));
+		board[8][8].setPiece(new Lance(2));
 		//Add Kings to initial setup
-			board[0][4].setPiece(new King(1));
-			board[8][4].setPiece(new King(2));
+		board[0][4].setPiece(new King(1));
+		board[8][4].setPiece(new King(2));
 		//Add Gold to initial setup
-			board[0][3].setPiece(new Gold(1));
-			board[0][5].setPiece(new Gold(1));
-			board[8][3].setPiece(new Gold(2));
-			board[8][5].setPiece(new Gold(2));
+		board[0][3].setPiece(new Gold(1));
+		board[0][5].setPiece(new Gold(1));
+		board[8][3].setPiece(new Gold(2));
+		board[8][5].setPiece(new Gold(2));
 		//Add Silvers to initial setup
-			board[0][2].setPiece(new Silver(1));
-			board[0][6].setPiece(new Silver(1));
-			board[8][2].setPiece(new Silver(2));
-			board[8][6].setPiece(new Silver(2));
+		board[0][2].setPiece(new Silver(1));
+		board[0][6].setPiece(new Silver(1));
+		board[8][2].setPiece(new Silver(2));
+		board[8][6].setPiece(new Silver(2));
 		//Add Knights to initial setup
-			board[0][1].setPiece(new Knight(1));
-			board[0][7].setPiece(new Knight(1));
-			board[8][1].setPiece(new Knight(2));
-			board[8][7].setPiece(new Knight(2));
+		board[0][1].setPiece(new Knight(1));
+		board[0][7].setPiece(new Knight(1));
+		board[8][1].setPiece(new Knight(2));
+		board[8][7].setPiece(new Knight(2));
 		//Add Rooks to initial setup
-			board[1][1].setPiece(new Rook(1));
-			board[7][7].setPiece(new Rook(2));
+		board[1][1].setPiece(new Rook(1));
+		board[7][7].setPiece(new Rook(2));
 		//Added Bishops to initial setup
-			board[1][7].setPiece(new Bishop(1));
-			board[7][1].setPiece(new Bishop(2));
+		board[1][7].setPiece(new Bishop(1));
+		board[7][1].setPiece(new Bishop(2));
 	}
-	
+
 	public void movePiece(Square from, Square to) throws Exception {
 		Piece fromPiece = from.getPiece();
 		if(fromPiece.canMove(from, to, this)) {
-				from.setPiece(null);
-				to.setPiece(null);
-				to.setPiece(fromPiece);
-		} else {
+			from.setPiece(null);
+			to.setPiece(null);
+			to.setPiece(fromPiece);
+		} 
+		else if(from.getC() == 100 && from.getR() == 100){
+			from.setPiece(null);
+			to.setPiece(null);
+			to.setPiece(fromPiece);
+		} 
+		else {
 			throw new Exception();
 		}
 	}
-	
+
 	public Square getSquare(int r, int c) {
 		return board[r][c];
 	}
-	
+
 	public String toString() {
 		String ret = "";
 		for(int r=0;r<board.length;r++) {
